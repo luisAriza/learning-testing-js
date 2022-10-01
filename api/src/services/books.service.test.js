9ed5f049c13e52a28d5927af651cb6d8198d8d9a
Luis Ariza <luismanuelariza@hotmail.com>
2022-10-01 17:37:52 -0500

    Mocking

--- api/src/services/books.service.test.js
+++ api/src/services/books.service.test.js
@@ -1,20 +1,39 @@
-const BooksService = require("./books.service");
+const BooksService = require('./books.service');
 
-describe("Test for BooksServices", () => {
+const fakeBooks = [
+  {
+    _id: 1,
+    name: 'Harry Potter',
+  },
+];
+
+const MongoLibStub = {
+  getAll: () => [...fakeBooks],
+  create: () => {},
+};
+
+jest.mock('../lib/mongo.lib', () => jest.fn().mockImplementation(() => MongoLibStub));
+
+describe('Test for BooksServices', () => {
   let service;
-
   beforeEach(() => {
     service = new BooksService();
+    jest.clearAllMocks();
   });
 
-  describe("Test for getBooks", () => {
-    test("Should return a list book", async () => {
+  describe('Test for getBooks', () => {
+    test('Should return a list book', async () => {
       // Arrange
       // Act
       const books = await service.getBooks();
       console.log(books);
       // Assert
-      expect(books.length).toEqual();
+      expect(books.length).toEqual(1);
+    });
+    test('Should return a list book', async () => {
+      const books = await service.getBooks();
+      console.log(books);
+      expect(books[0].name).toEqual('Harry Potter');
     });
   });
 });
